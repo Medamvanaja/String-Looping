01174b07da0562b2cead927b45de0a0b8cfa1e5b
Medamvanaja <162460110+Medamvanaja@users.noreply.github.com>
2024-06-06 18:46:03 +0530

    String Looping.java

---
+++ String Looping.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      String[] arr1 =new String[] {"10","58","96","75","80","63","755","15"};
+      String[] arr2 =new String[] {"Welcome","to","Java"};
+      for(String x:arr1)
+      {
+        System.out.print(x +",");
+      }
+      System.out.println();
+      for(String x:arr2)
+      {
+        System.out.print(x +" ");
+      }
+  }
+}
